c27069df5043bd2ded9b0c48f3e93958d5e5b9b9
pavlodorosh <dpm1987@gmail.com>
2023-01-30 21:52:38 +0200

    show data sql

--- index23.php
+++ index23.php
@@ -19,45 +19,110 @@ mysqli_num_rows() - Отримує число рядів у результуюч
 mysqli_affected_rows() - Отримує кількість рядків, порушених попередньою операцією MySQL. Повертає кількість рядків, які торкнулися останнім INSERT, UPDATE, REPLACE або DELETE запитом. Ціле число, більше нуля, означає кількість порушених чи отриманих рядків. Нуль означає, що запитом виду UPDATE не оновлено жодного запису, або що жодний рядок не відповідає умові WHERE у запиті, або що запит ще не було виконано. Значення -1 свідчить про те, що запит повернув помилку.
 
 mysqli_error() - Повертає рядок із описом останньої помилки.
+
+mysqli_real_escape_string() - Екранує спеціальні символи в рядку, що використовується в SQL-запиті, беручи до уваги кодування з'єднання
 */
 
+// $db = mysqli_connect('127.0.0.1', 'root1', 'root', 'up', 3306 ) or die ('error connection');
 $db = @mysqli_connect('127.0.0.1', 'root', 'root', 'up', 3306 ) or die ('error connection');
+
+// echo '<pre>';
+// var_dump($db);
+// echo '</pre>';
+
 if(!$db) die(mysqli_connect_error());
-mysqli_set_charset($db, "utf8") or die('no connection db');
 
-$res = mysqli_query($db, "SELECT * FROM `customers`");
-var_dump($res);
+// mysqli_set_charset($db, "utf8") or die('no connection db');
+// $res = mysqli_query($db, "SELECT * FROM `customers`");
+// var_dump($res);
 
 
-//SHOW
+// SHOW
 $sql = "SELECT cname, city FROM `customers`";
 $result = $db->query($sql);
+$res = mysqli_query($db, $sql);
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+// echo '<pre>';
+// var_dump($res);
+// echo '</pre>';
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<br> name: ". $row["cname"]. " - city: ". $row["city"].  "<br>";
-    }
-} else {
-    echo "0 results";
-}
+
+// echo 'Відсутні параметри';
+// $date = mysqli_fetch_all($res);
+// $date = mysqli_fetch_all($res, MYSQLI_ASSOC);
+// $date = mysqli_fetch_all($res, MYSQLI_NUM);
+// $date = mysqli_fetch_all($res, MYSQLI_BOTH);
+// echo '<pre>';
+// print_r($date);
+// echo '</pre>';
+// echo '<br>';
+// echo '<br>';
+// echo '<br>';
+// echo '<br>';
+
+// $data2 = [];
+// while( $row = mysqli_fetch_assoc($res)){
+//     $data2[$row['cname']] = $row;
+// }
+// echo '<pre>';
+// print_r($data2);
+// echo '</pre>';
+
+// echo mysqli_num_rows($res);
+
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         echo "<br> name: ". $row["cname"]. " - city: ". $row["city"].  "<br>";
+//     }
+// } else {
+//     echo "0 results";
+// }
+
+
 
 // INSERT
-// $insert = "INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`, `text`) VALUES
-// (2088, 'Andrey', 'London', 100, 1001, 'php')";
+$insert = "INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`, `text`) VALUES
+(2125, 'Andrey', 'London', 100, 1001, 'php')";
 
-// $res_insert = mysqli_query($db, $insert);
-// if($res_insert) echo "OK Update";
-// else echo "Error update";
+$res_insert = mysqli_query($db, $insert);
+if($res_insert) echo "OK insert";
+else echo "Error insert";
+echo '<br>';
+echo 'mysqli_error - '.mysqli_error($db);
+echo '<br>';
+echo 'mysqli_affected_rows - '.mysqli_affected_rows($db);
+echo '<br>';
+echo '<br>';
+echo '<br>';
 
-// echo mysqli_error($db);
-
-
-//UPDATE
-// $update = "UPDATE `customers` SET cname = 'UPDATE NAME' WHERE cnum = 2085";
-// $resUpdate = mysqli_query($db, $update) or die ( mysqli_error($db));
-
+// UPDATE
+$update = "UPDATE `customers` SET cname = 'UPDATE NAME' WHERE cnum = 2125";
+$resUpdate = mysqli_query($db, $update) or die ( mysqli_error($db));
+echo '<pre>';
+print_r($resUpdate);
+echo '</pre>';
 
 
 //DELETE
-// $delete = "DELETE FROM  `customers` WHERE cnum = 2085";
-// $resdelete = mysqli_query($db, $delete) or die ( mysqli_error($db));
+$delete = "DELETE FROM  `customers` WHERE cnum = 2085";
+$resdelete = mysqli_query($db, $delete) or die ( mysqli_error($db));
+echo '<pre>';
+print_r($resdelete);
+echo '</pre>';
+
+
+$str= "Ім'я";
+$str = mysqli_real_escape_string($db, $str);
+$insert = "INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`, `text`) VALUES
+(2128, '$str', 'London', 100, 1001, 'php')";
+$res_insert = mysqli_query($db, $insert);
+if($res_insert) echo "OK insert";
+else echo "Error insert";
+echo '<br>';
+echo 'mysqli_error - '.mysqli_error($db);
